bba7a0af3a29140d55d72f7c7251af772b50d512
Dani <martinezcasabuena@gmail.com>
2017-09-15 16:41:15 +0200

    Adaptar estructura web

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
-<?php 
-	require_once("view/inc/header.html"); 
+<?php
 
-	
+	require_once("view/inc/header.html");
 	include 'utils/utils.inc.php';
-	
+
 	session_start();
 
 	if (!isset($_GET['module'])) {
@@ -11,9 +10,10 @@
 	} else	if ( (isset($_GET['module'])) && (!isset($_GET['view'])) ){
 		require_once("modules/".$_GET['module']."/controller/controller_".$_GET['module'].".class.php");
 	}
-	
+
 	if ( (isset($_GET['module'])) && (isset($_GET['view'])) ) {
 		require_once("modules/".$_GET['module']."/view/".$_GET['view'].".php");
-	} 
-		
+	}
+
 	require_once("view/inc/footer.html");
+	require_once("view/inc/menu.php");
---
+++ view/inc/menu.php
@@ -0,0 +1,18 @@
+<header id="header" class="alt">
+	<h1><a href="index.html">Spectral</a></h1>
+	<nav id="nav">
+		<ul>
+			<li class="special">
+				<a href="#menu" class="menuToggle"><span>Menu</span></a>
+				<div id="menu">
+					<ul>
+						<li><a href="index.php?module=main">Home</a></li>
+						<li><a href="index.php?module=generic">Generic</a></li>
+						<li><a href="index.php?module=elements">Elements</a></li>
+						<li><a href="#">Sign Up</a></li>
+						<li><a href="#">Log In</a></li>
+					</ul>
+				</div>
+			</li>
+		</ul>
+	</nav>
