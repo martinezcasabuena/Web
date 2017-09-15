2beb6699da78b5f71c6232ea54432ec420dcccb5
Dani <martinezcasabuena@gmail.com>
2017-09-15 12:49:55 +0000

    comentario

---
+++ utils/utils.inc.php
@@ -0,0 +1,12 @@
+	<?php
+	    function debugPHP($array){
+			echo "<pre>";
+			print_r($array);
+			echo "</pre><br>";
+			//die(); no va
+		}
+
+		function redirect($url){
+			die('<script>top.location.href="'.$url .'";</script>');
+		}
+
